e38fcb3847ca4e51d7d8ed5f2a2e974ccff06f38
sameh-k <samehkhater.a@gmail.com>
2024-03-27 05:07:37 +0200

    Add background image

--- src/components/UpcomingWeather.js
+++ src/components/UpcomingWeather.js
@@ -1,5 +1,14 @@
 import React from 'react'
-import {FlatList, SafeAreaView, StyleSheet, Text, View} from 'react-native'
+import {
+  FlatList,
+  Image,
+  ImageBackground,
+  SafeAreaView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native'
 import {Feather} from '@expo/vector-icons'
 
 const DATA = [
@@ -44,11 +53,11 @@ const DATA = [
 const Item = props => {
   const {dt_txt, min, max, condition} = props
   return (
-    <View>
+    <View style={styles.item}>
       <Feather name="sun" size={50} color={'white'} />
-      <Text>{dt_txt}</Text>
-      <Text>{min}</Text>
-      <Text>{max}</Text>
+      <Text style={styles.date}>{dt_txt}</Text>
+      <Text style={styles.temp}>{min}</Text>
+      <Text style={styles.temp}>{max}</Text>
     </View>
   )
 }
@@ -67,18 +76,45 @@ const UpcomingWeather = () => {
 
   return (
     <SafeAreaView style={styles.container}>
-      <Text>Upcoming Weather</Text>
-      <FlatList
-        data={DATA}
-        renderItem={renderItem}
-        keyExtractor={item => item.dt_txt}
-      />
+      <ImageBackground
+        source={require('../../assets/clouds.jpg')}
+        style={styles.Image}>
+        <Text>Upcoming Weather</Text>
+        <FlatList
+          data={DATA}
+          renderItem={renderItem}
+          keyExtractor={item => item.dt_txt}
+        />
+      </ImageBackground>
     </SafeAreaView>
   )
 }
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight || 0,
+    backgroundColor: 'royalblue'
+  },
+  item: {
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    borderWidth: 5,
+    backgroundColor: 'pink'
+  },
+  temp: {
+    color: 'white',
+    fontSize: 20
+  },
+  date: {
+    color: 'white',
+    fontSize: 15
+  },
+  image: {
     flex: 1
   }
 })
